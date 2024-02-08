484ab65e412558e18e7ed7bef6e35c801519b4bc
basya <144015194+varyaqt@users.noreply.github.com>
2024-02-08 12:50:31 +0300

    Create dynarr.hpp

---
+++ prj.lab/dynarr/dynarr.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef DYNARR_HPP
+#define DYNARR_HPP
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cstddef>
+#include <stdexcept>
+
+class DynArr {
+public:
+    DynArr() = default;
+    DynArr(const DynArr& d);
+    DynArr(std::ptrdiff_t size);
+    ~DynArr() { delete[] data_; }
+
+    [[nodiscard]] DynArr& operator=(const DynArr& d) noexcept;
+
+    [[nodiscard]] std::ptrdiff_t size() const noexcept;
+    void resize(std::ptrdiff_t size);
+
+    [[nodiscard]] float& operator[](const std::ptrdiff_t i);
+    [[nodiscard]] const float& operator[](const std::ptrdiff_t i) const;
+
+private:
+    std::ptrdiff_t size_ = 0;
+    std::ptrdiff_t capacity_ = 0;
+
+    float* data_ = nullptr;
+};
+
+#endif
