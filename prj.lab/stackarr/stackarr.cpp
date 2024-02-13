5e4710f542f76c1dd59146ecab06235758951a9b
basya <144015194+varyaqt@users.noreply.github.com>
2024-02-13 09:47:13 +0300

    Update stackarr.cpp

--- prj.lab/stackarr/stackarr.cpp
+++ prj.lab/stackarr/stackarr.cpp
@@ -1,60 +1,51 @@
-#include "stackarr.hpp"
+#include "complex/complex.hpp"
+#include "stackarr/stackarr.hpp"
 
-StackArr::StackArr() {
-    capacity_ = 5;
-    data_ = new Complex[capacity_];
+#include <algorithm>
+#include <stdexcept>
+
+bool StackArr::isEmpty() const noexcept {
+    return 0 <= i_top_;
 }
 
-StackArr::StackArr(const StackArr& obj) {
-    size_ = obj.size_;
-    capacity_ = obj.capacity_;
-    data_ = new Complex[size_];
-    std::copy(obj.data_, obj.data_ + obj.size_, data_);
+void StackArr::pop() noexcept {
+    if (!isEmpty()) {
+        i_top_ -= 1;
+    }
+}
+void StackArr::clear() noexcept {
+    i_top_ = -1;
+}
+
+void StackArr::push(const Complex& s) {
+    if (data_ == nullptr) {
+        size_ = 10;
+        data_ = new Complex[size_];
+    }
+    else if (size_ == i_top_ + 1) {
+        auto buf = new Complex(size_ * 2);
+        std::copy(data_, data_ + size_, buf);
+        std::swap(data_, buf);
+        delete[] buf;
+        size_ *= 2;
+    }
+    data_[++i_top_] = s;
+}
+
+const Complex& StackArr::top() const{
+    if (i_top_ < 0) {
+        throw std::logic_error("StackArr - try get top form empty stack.");
+    }
+    return data_[i_top_];
 }
 
 StackArr& StackArr::operator=(const StackArr& rhs) noexcept {
-    if (rhs.size_ > capacity_) {
-        capacity_ = rhs.size_;
+    if (rhs.size_ > size_) {
+        size_ = rhs.size_;
         delete[] data_;
         data_ = new Complex(rhs.size_);
     }
     std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
     size_ = rhs.size_;
     return *this;
-}
-
-bool StackArr::isEmpty() noexcept {
-    if (size_ == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-void StackArr::pop() noexcept {
-    if (size_ > 0) {
-        size_--;
-    }
-}
-
-void StackArr::push(const Complex& s) {
-    if (size_ + 1 > capacity_) {
-        Complex* newData = new Complex[capacity_ + 5];
-        std::copy(data_, data_ + size_, newData);
-        delete[] data_;
-        data_ = newData;
-        capacity_ += 5;
-    }
-    size += 1;
-    data_[size_ - 1] = s;
-}
-
-Complex& StackArr::top() {
-    if (size_ > 0) {
-        return data_[size_ - 1];
-    }
-    else {
-        throw std::out_of_range("Error! Now stack is empty!\n")
-    }
 }
