34dcac000af2c7ebd4ba18d53a002c8a33370b5f
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-06 21:27:34 +0300

    Update complex.cpp

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -1,4 +1,4 @@
-#include "complex.hpp"
+#include "complex/complex.hpp"
 
 Complex::Complex(const double real)
 	: Complex(real, 0.0)
@@ -12,58 +12,46 @@ Complex::Complex(const double real, const double imaginary)
 }
 
 //addition
-Complex operator+(const Complex& lhs, const Complex& rhs) {
-	return Complex(lhs.re + rhs.re, lhs.im + rhs.im);
-}
 Complex& Complex::operator+=(const Complex& rhs) {
 	re += rhs.re;
 	im += rhs.im;
 	return *this;
 }
+Complex operator+(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) += rhs; }
+Complex operator+(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) += rhs; }
+Complex operator+(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) += rhs; }
 
 //subtraction
-Complex operator-(const Complex& lhs, const Complex& rhs) {
-	return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
-}
 Complex& Complex::operator-=(const Complex& rhs) {
 	re -= rhs.re;
 	im -= rhs.im;
 	return *this;
 }
+Complex operator-(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) -= rhs; }
+Complex operator-(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) -= rhs; }
+Complex operator-(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) -= rhs; }
 
 //multiplication
-Complex operator*(const Complex& lhs, const Complex& rhs) {
-	return Complex(lhs.re * rhs.re - lhs.im * rhs.im, lhs.re * rhs.im + lhs.im * rhs.re);
-}
 Complex& Complex::operator*=(const Complex& rhs) {
 	double temp = re * rhs.re - im * rhs.im;
 	im = re * rhs.im + im * rhs.re;
 	re = temp;
 	return *this;
 }
+Complex operator*(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) *= rhs; }
+Complex operator*(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) *= rhs; }
+Complex operator*(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) *= rhs; }
 
 //division
-Complex operator/(const Complex& lhs, const Complex& rhs) {
-	return Complex((lhs.re * rhs.re + lhs.im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im), (rhs.re * lhs.im - lhs.re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im));
-}
 Complex& Complex::operator/=(const Complex& rhs) {
 	double temp = (re * rhs.re + im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
 	im = (rhs.re * im - re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
 	re = temp;
 	return *this;
 }
-
-//abs
-double absolute(const Complex& lhs) {
-	return (sqrt(lhs.re * lhs.re + lhs.im * lhs.im));
-}
-
-//pow
-Complex pow(Complex& lhs, const int degree) {
-	Complex term = lhs;
-	for (int i = 1; i < degree; ++i) lhs *= term;
-	return lhs;
-}
+Complex operator/(const Complex& lhs, const Complex& rhs) { return Complex(lhs) /= rhs; }
+Complex operator/(const Complex& lhs, const double rhs) { return Complex(lhs) /= rhs; }
+Complex operator/(const double lhs, const Complex& rhs) { return Complex(lhs) /= rhs; }
 
 //conjugate
 Complex conjugate(Complex& c) {
@@ -71,15 +59,13 @@ Complex conjugate(Complex& c) {
 	return cc;
 }
 
-
-std::ostream& Complex::writeTo(std::ostream& ostrm) const
+std::ostream& Complex::WriteTo(std::ostream& ostrm) const
 {
 	ostrm << leftBrace << re << separator << im << rightBrace;
 	return ostrm;
 }
 
-
-std::istream& Complex::readFrom(std::istream& istrm)
+std::istream& Complex::ReadFrom(std::istream& istrm)
 {
 	char leftBrace(0);
 	double real(0.0);
