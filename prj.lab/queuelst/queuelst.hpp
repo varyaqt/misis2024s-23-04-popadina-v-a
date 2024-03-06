d551a15a225d3aedbd41b26481b3dd6b07821d8c
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-06 20:36:07 +0300

    Create queuelst.hpp

---
+++ prj.lab/queuelst/queuelst.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef QUEUELST_QUEUELST
+#define QUEUELST_QUEUELST
+
+#include <complex/complex.hpp>
+
+#include <cstddef>
+
+class QueueLst {
+public:
+    QueueLst() = default;
+    QueueLst(const QueueLst&) = default;
+    ~QueueLst() = default;
+    [[nodiscard]] QueueLst& operator=(const QueueLst&) = default;
+    [[nodiscard]] bool IsEmpty() const noexcept;
+    void Pop() noexcept;
+    void Push(const Complex& val);
+    [[nodiscard]] Complex& Top();
+    [[nodiscard]] const Complex& Top() const;
+    void Clear() noexcept;
+private:
+    struct Node {
+        Complex val;
+        Node* next = nullptr;
+    };
+    Node* head_ = nullptr;   //!< 
+    Node* tail_ = nullptr;   //!< 
+};
+#endif
