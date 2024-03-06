ff35e1c0045ee53a3eda0986133f105cdcbcdd8e
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-06 21:46:07 +0300

    Update test_complex.cpp

--- prj.test/test_complex.cpp
+++ prj.test/test_complex.cpp
@@ -1,4 +1,4 @@
-#include <complex/complex.hpp>
+#include "complex/complex.hpp"
 
 void test() {
 	Complex c1(0, 0);
@@ -10,70 +10,27 @@ void test() {
 	std::cin >> c1;
 	std::cout << "Input complex num 2: ";
 	std::cin >> c2;
-	Complex c1memory = c1;
-	Complex checker(0, 0);
+	std::cout << "test 1.1: addition" << std::endl;
+	std::cout << c1 << "+" << c2 << "=" << c1 + c2 << std::endl;
+	std::cout << c1 << "+" << real << "=" << c1 + real << std::endl;
+	std::cout << real << "+" << c2 << "=" << real + c2 << std::endl;
 
-	std::cout << "CONJUGATE TO THE FIRST NUMBER: " << conjugate(c1) << std::endl;
-	std::cout << "CONJUGATE TO THE SECOND NUMBER: " << conjugate(c2) << std::endl;
+	std::cout << "test 1.2: substraction" << std::endl;
+	std::cout << c1 << "-" << c2 << "=" << c1 - c2 << std::endl;
+	std::cout << c1 << "-" << real << "=" << c1 - real << std::endl;
+	std::cout << real << "-" << c2 << "=" << real - c2 << std::endl;
 
-	std::cout << "test1.1: addition" << std::endl << "operator+: " << c1 + c2 << std::endl;
-	checker = c1 + c2;
-	c1 += c2;
-	std::cout << "operator+=: " << c1 << std::endl;
-	std::cout << std::endl;
-	c1 = c1memory;
-	std::cout << "test1.2: addition with real" << std::endl << "operator+: " << c1 + real << std::endl;
-	checker = c1 + real;
-	c1 += real;
-	std::cout << "operator+=: " << c1 << std::endl;
-	std::cout << std::endl;
-	c1 = c1memory;
+	std::cout << "test 1.3: multiplication" << std::endl;
+	std::cout << c1 << "*" << c2 << "=" << c1 * c2 << std::endl;
+	std::cout << c1 << "*" << real << "=" << c1 * real << std::endl;
+	std::cout << real << "*" << c2 << "=" << real * c2 << std::endl;
 
-	std::cout << "test2.1: subtraction" << std::endl << "operator-: " << c1 - c2 << std::endl;
-	checker = c1 - c2;
-	c1 -= c2;
-	std::cout << "operator-=: " << c1 << std::endl;
-	std::cout << std::endl;
-	c1 = c1memory;
-	std::cout << "test2.2: subtraction with real" << std::endl << "operator-: " << c1 - real << std::endl;
-	checker = c1 - real;
-	c1 -= real;
-	std::cout << "operator-=: " << c1 << std::endl;
-	std::cout << std::endl;
-	c1 = c1memory;
 
-	std::cout << "test3.1: multiplication" << std::endl << "operator*: " << c1 * c2 << std::endl;
-	checker = c1 * c2;
-	c1 *= c2;
-	std::cout << "operator*=: " << c1 << std::endl;
-	std::cout << std::endl;
-	c1 = c1memory;
-	std::cout << "test3.2: multiplication with real" << std::endl << "operator*: " << c1 * real << std::endl;
-	checker = c1 * real;
-	c1 *= real;
-	std::cout << "operator*=: " << c1 << std::endl;
-	std::cout << std::endl;
-	c1 = c1memory;
+	std::cout << "CONJUGATE TO THE" << c1 << ": " << conjugate(c1) << std::endl;
+	std::cout << "CONJUGATE TO THE" << c2 << ": " << conjugate(c2) << std::endl;
 
-	std::cout << "test4: division" << std::endl;
-	if (c2 == Complex(0)) std::cout << "BAD! DIVISION BY ZERO";
-	else {
-		std::cout << "operator/: " << c1 / c2 << std::endl;
-		checker = c1 / c2;
-		c1 /= c2;
-		std::cout << "operator/=: " << c1 << std::endl;
-		std::cout << std::endl;
-		c1 = c1memory;
-	}
-	std::cout << "THE MODULE OF THE FIRST NUMBER: " << absolute(c1) << std::endl;
-	std::cout << "THE MODULE OF THE SECOND NUMBER: " << absolute(c2) << std::endl;
-	std::cout << "ENTER THE DEGREE: ";
-	int degree;
-	std::cin >> degree;
-	std::cout << "RESULT: " << pow(c1, degree) << std::endl;
-	std::cout << "FIRST NUM == SECOND NUM: " << static_cast<bool>(c1 == c2) << std::endl;
-	std::cout << "FIRST NUM != SECOND NUM: " << static_cast<bool>(c1 != c2) << std::endl;
-}
+	std::cout << c1 << " == " << c2 << ": " static_cast<bool>(c1 == c2) << std::endl;
+	std::cout << c1 << " != " << c2 << ": " static_cast<bool>(c1 != c2) << std::endl;
 
 int main() {
 	test();
