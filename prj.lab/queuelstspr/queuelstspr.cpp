36d97c226353a195d67997ad21b18cb655ac7d77
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-10 21:09:05 +0300

    Update queuelstspr.cpp

--- prj.lab/queuelstspr/queuelstspr.cpp
+++ prj.lab/queuelstspr/queuelstspr.cpp
@@ -3,18 +3,18 @@
 #include <algorithm>
 #include <stdexcept>
 
-bool IsEmpty() noexcept {
+bool QueueLstsPr::IsEmpty() noexcept {
 	return head_ == nullptr;
 }
 
-void Clear() {
+void QueueLstsPr::Clear() {
 	if (!IsEmpty()) {
 		head_ = nullptr;
 		tail_ = nullptr;
 	}
 }
 
-void Sort(double* data_) {
+void QueueLstsPr::Sort(double* data_) {
 	size = data_.size();
 	for (int i = 0, i != size; ++i) {
 		for (int j = 0; j < size - 1; ++j) {
@@ -24,14 +24,14 @@ void Sort(double* data_) {
 		}
 	}
 }
-double Top() {
+double QueueLstsPr::Top() {
 	if (IsEmpty()) {
 		throw std::logic_error("QueueLstsPr - try get top from empty queue.");
 	}
 	return head_->val;
 }
 
-void Pop() {
+void QueueLstsPr::Pop() {
 	if (!IsEmpty()) {
 		Node* deleted = head_;
 		head_ = head_->next;
@@ -42,7 +42,8 @@ void Pop() {
 	}
 }
 
-void Push(const double& val) {
+
+void QueueLstsPr::Push(const double& val) {
 	if (IsEmpty()) {
 		tail_ = new Node{ val };
 		head_ = tail_;
@@ -53,6 +54,41 @@ void Push(const double& val) {
 	}
 	data_.push_back(val);
 	Sort(double* data_);
+}
+QueueLstsPr& QueueLstsPr::operator=(const QueueLst& rhs) {
+	if (this != &rhs) {
+		Node* vs = rhs.head_;
+		Node* tvs = head_;
+		Node* pr;
+		while (vs != nullptr) {
+			if (tvs != nullptr) {
+				tvs->v = vs->v;
+			}
+			else {
+				Node* newNode = new Node(vs->v);
+				if (head_ == nullptr) {
+					head_ = newNode;
+					tail_ = newNode;
+				}
+				else {
+					tail_->next = newNode;
+					tail_ = tail_->next;
+				}
+				tvs = newNode;
+			}
+			pr = tvs;
+			tvs = tvs->next;
+			vs = vs->next;
+		}
+		while (tvs != nullptr) {
+			Node* delete_future = tvs;
+			tvs = tvs->next;
+			delete delete_future;
+		}
+		tail_ = pr;
+		tail_->next = nullptr;
+	}
+	return *this;
 }
 
 /*QueueLstsPr& operator=(const QueueLstsPr& obj);*/
