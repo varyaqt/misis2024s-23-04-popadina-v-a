d516fdcce52fab712131f886e0d5c558172ae6db
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-12 09:22:12 +0300

    add &&

--- prj.lab/queuelst/queuelst.cpp
+++ prj.lab/queuelst/queuelst.cpp
@@ -1,6 +1,24 @@
-#include <queuelst/queuelst.hpp>
+#include "queuelst/queuelst.hpp"
 
 #include <stdexcept>
+
+QueueLst::QueueLst(QueueLst&& obj) noexcept {
+    head_ = obj.head_;
+    tail_ = obj.tail_;
+    obj.head_ = nullptr;
+    obj.tail_ = nullptr;
+}
+
+QueueLst& QueueLst::operator=(QueueLst&& rhs) noexcept {
+    if (this != &rhs) {
+        Clear();
+        head_ = rhs.head_;
+        tail_ = rhs.tail_;
+        rhs.head_ = nullptr;
+        rhs.tail_ = nullptr;
+    }
+    return *this;
+}
 
 bool QueueLst::IsEmpty() const noexcept {
     return nullptr == head_;
@@ -30,7 +48,7 @@ void QueueLst::Push(const Complex& val) {
 
 Complex& QueueLst::Top() {
     if (IsEmpty()) {
-        throw std::logic_error("QueueLst - try get top form empty queue.");
+        throw std::logic_error("QueueLst - try get top from empty queue.");
     }
     return head_->val;
 }
@@ -46,4 +64,40 @@ void QueueLst::Clear() noexcept {
     while (!IsEmpty()) {
         Pop();
     }
+}
+
+QueueLst& QueueLst::operator=(const QueueLst& rhs) {
+    if (this != &rhs) {
+        Node* vs = rhs.head_;
+        Node* tvs = head_;
+        Node* pr;
+        while (vs != nullptr) {
+            if (tvs != nullptr) {
+                tvs->v = vs->v;
+            }
+            else {
+                Node* newNode = new Node(vs->v);
+                if (head_ == nullptr) {
+                    head_ = newNode;
+                    tail_ = newNode;
+                }
+                else {
+                    tail_->next = newNode;
+                    tail_ = tail_->next;
+                }
+                tvs = newNode;
+            }
+            pr = tvs;
+            tvs = tvs->next;
+            vs = vs->next;
+        }
+        while (tvs != nullptr) {
+            Node* delete_future = tvs;
+            tvs = tvs->next;
+            delete delete_future;
+        }
+        tail_ = pr;
+        tail_->next = nullptr;
+    }
+    return *this;
 }
