12c2b457d70f7c0569515b53fa5fb5f060353661
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-25 13:38:34 +0300

    Update complex.cpp

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -11,6 +11,63 @@ Complex::Complex(const double real, const double imaginary)
 {
 }
 
+Complex Complex::operator-() const noexcept {
+    return Complex(-re, -im);
+}
+
+
+double abs(const Complex& rhs) noexcept
+{
+    double ans = std::pow((std::pow(rhs.re, 2) + std::pow(rhs.im, 2)), 0.5);
+    return ans;
+}
+
+Complex pow(const Complex& lhs, int n)
+{
+    const Complex c0 = { 0,0 };
+    try {
+        if (lhs == c0 && n != 0) {
+            return { 0,0 };
+        }
+        else if (lhs == c0 && n == 0) {
+            throw std::invalid_argument("Zero to the zero degree!\n");
+        }
+        else if (n == 0) {
+            return Complex(1);
+        }
+        else if (n > 0) {
+            return lhs * pow(lhs, --n);
+        }
+        else if (n < 0) {
+            Complex olhs = (1.0 / lhs);
+            return olhs * pow(lhs, ++n);
+        }
+    }
+    catch (std::invalid_argument& e) {
+        std::cout << e.what();
+        exit(1);
+    }
+}
+
+bool operator==(const Complex& lhs, const Complex& rhs) noexcept {
+    double constexpr eps = 2 * std::numeric_limits<double>::epsilon();
+    if (std::abs(lhs.im - rhs.im) <= eps && std::abs(lhs.re - rhs.re) <= eps) return 1;
+    else return 0;
+}
+
+bool operator==(const Complex& lhs, const double rhs) noexcept { return operator==(lhs, Complex(rhs)); }
+bool operator==(const double lhs, const Complex& rhs) noexcept { return operator==(Complex(lhs), rhs); }
+bool operator!=(const Complex& lhs, const Complex& rhs) noexcept { return !operator==(lhs, rhs); }
+bool operator!=(const Complex& lhs, const double rhs) noexcept { return !operator==(lhs, Complex(rhs)); }
+bool operator!=(const double lhs, const Complex& rhs) noexcept { return !operator==(Complex(lhs), rhs); }
+
+Complex operator!(const Complex& rhs) noexcept {
+    Complex c;
+    c.re = rhs.re;
+    c.im = -rhs.im;
+    return c;
+}
+
 //addition
 Complex& Complex::operator+=(const Complex& rhs) {
 	re += rhs.re;
@@ -43,21 +100,30 @@ Complex operator*(const Complex& lhs, const double rhs) noexcept { return Comple
 Complex operator*(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) *= rhs; }
 
 //division
-Complex& Complex::operator/=(const Complex& rhs) {
-	double temp = (re * rhs.re + im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
-	im = (rhs.re * im - re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
-	re = temp;
-	return *this;
+
+Complex& Complex::operator/=(const Complex& rhs)
+{
+    const Complex c0 = { 0,0 };
+    try {
+        if (rhs == c0) {
+            throw std::invalid_argument("Division by zero\n");
+        }
+        else {
+            auto re1 = re, im1 = im;
+            re = (re * rhs.re + im * rhs.im) / (std::pow(rhs.re, 2) + std::pow(rhs.im, 2));
+            im = (im1 * rhs.re - re1 * rhs.im) / (std::pow(rhs.re, 2) + std::pow(rhs.im, 2));
+            return *this;
+        }
+    }
+    catch (std::invalid_argument& e) {
+        std::cout << e.what();
+        exit(1);
+    }
 }
 Complex operator/(const Complex& lhs, const Complex& rhs) { return Complex(lhs) /= rhs; }
 Complex operator/(const Complex& lhs, const double rhs) { return Complex(lhs) /= rhs; }
 Complex operator/(const double lhs, const Complex& rhs) { return Complex(lhs) /= rhs; }
 
-//conjugate
-Complex conjugate(Complex& c) {
-	Complex cc(c.re, -1 * c.im);
-	return cc;
-}
 
 std::ostream& Complex::WriteTo(std::ostream& ostrm) const
 {
