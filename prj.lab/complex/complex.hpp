5eb226872cfa17f30da3bdec276a988f1b0459f4
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-25 13:28:20 +0300

    Update complex.hpp

--- prj.lab/complex/complex.hpp
+++ prj.lab/complex/complex.hpp
@@ -1,17 +1,17 @@
+#pragma once
 #ifndef COMPLEX_HPP
 #define COMPLEX_HPP
 
 #include <iosfwd>
 
-
 struct Complex {
 	Complex() {}
-	Complex() = default;
-	Complex(const Complex&) = defailt;
-	Complex(Complex&&) = default;
+	[[nodiscard]] Complex() = default;
+	[[nodiscard]] Complex(const Complex&) = default;
+	[[nodiscard]]  Complex(Complex&&) = default;
 	~Complex() = default;
-	explicit Complex(const double real) : re(real) { }
-	Complex(const double real, const double imaginary) : re(real), im(imaginary) {}
+	[[nodiscard]] explicit Complex(const double real) : Complex (real, 0.0) { }
+	[[nodiscard]] Complex(const double real, const double imaginary) : re(real), im(imaginary) {}
 	Complex& operator=(const Complex&) = default;
 	Complex& operator=(Complex&&) = default;
 	[[nodiscard]] bool operator==(const Complex& rhs) const { return (re == rhs.re) && (im == rhs.im); }
@@ -19,6 +19,8 @@ struct Complex {
 	[[nodiscard]] bool operator!=(const Complex& rhs) const { return !operator==(rhs); }
 	[[nodiscard]] bool operator!=(const double rhs) const { return operator!=(Complex(rhs)); }
 
+	Complex& operator=(const Complex& rhs) = default;
+	Complex& operator=(Complex&&) = default;
 	Complex& operator+=(const Complex& rhs) noexcept;
 	Complex& operator+=(const double rhs) noexcept {
 		return operator+=(Complex(rhs));
@@ -38,8 +40,8 @@ struct Complex {
 	Complex& operator/=(const double rhs) {
 		return operator/=(Complex(rhs));
 	}
+	[[nodiscard]] Complex operator-() const noexcept;
 	void conjugate() { im = -im; }
-
 
 	[[nodiscard]] std::ostream& WriteTo(std::ostream& ostrm) const noexcept;
 	[[nodiscard]] std::istream& ReadFrom(std::istream& istrm) noexcept;
@@ -68,7 +70,10 @@ struct Complex {
 [[nodiscard]] Complex operator/(const Complex& lhs, const double rhs);
 [[nodiscard]] Complex operator/(const double lhs, const Complex& rhs);
 
-[[nodiscard]] Complex conjugate(const Complex& num);
+
+void test(const Complex& c1, const Complex& c2, const double c);
+
+bool testParse(const std::string& str);
 
 inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) noexcept {
 	return rhs.WriteTo(ostrm);
