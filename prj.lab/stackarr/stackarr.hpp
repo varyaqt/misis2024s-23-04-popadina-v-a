a5809c5bc844db2eac02eee5f61b00ca18e7e4e2
basya <144015194+varyaqt@users.noreply.github.com>
2024-03-30 13:41:27 +0300

    Update stackarr.hpp

--- prj.lab/stackarr/stackarr.hpp
+++ prj.lab/stackarr/stackarr.hpp
@@ -2,26 +2,31 @@
 #ifndef STACKARR_HPP
 #define STACKARR_HPP
 
+#include <iostream>
+#include <algorithm>
 #include <cstddef>
-
+#include <memory>
 #include "complex/complex.hpp"
 
 class Complex;
 
 class StackArr {
 public:
-    [[nodiscard]] StackArr() = default;
-    [[nodiscard]] StackArr(const StackArr& obj) = default;
+    [[nodiscard]] StackArr();
+    [[nodiscard]] StackArr(const StackArr& rhs);
+    [[nodiscard]] StackArr(StackArr&& rhs) noexcept;
     ~StackArr() = default;
     [[nodiscard]] StackArr& operator=(const StackArr& rhs) noexcept;
+    [[nodiscard]] StackArr& operator=(StackArr&& rhs) noexcept;
     bool IsEmpty() const noexcept;
     void Push(const Complex& s);
     void Pop() noexcept;
-    [[nodiscard]] const Complex& top() const;
+    [[nodiscard]] Complex& Top();
+    [[nodiscard]] const Complex& Top() const;
     void Clear() noexcept;
 private:
     std::ptrdiff_t size_ = 0; //!< число элементов в буфере
     std::ptrdiff_t i_top_ = -1; //!< индекс top элемента
-    Complex* data_ = nullptr; //!< элементы стека
+    std::unique_ptr<Complex[]> data_ = std::make_unique<Complex[]>(size_);
 };
 #endif
