0186032974d0554327a8011077a78b066c1e721a
basya <144015194+varyaqt@users.noreply.github.com>
2024-04-22 00:18:04 +0300

    Update dynarr.cpp

--- prj.lab/dynarr/dynarr.cpp
+++ prj.lab/dynarr/dynarr.cpp
@@ -1,4 +1,4 @@
-#include "dynarr/dynarr.hpp"
+#include "dynarr.hpp"
 
 DynArr::DynArr(const DynArr& d) {
     size_ = d.size_;
@@ -13,22 +13,22 @@ DynArr::DynArr(DynArr&& src) noexcept {
     std::swap(data_, src.data_);
 }
 
-DynArr::DynArr(const std::ptrdiff_t size){
+DynArr::DynArr(const std::ptrdiff_t size) {
     if (size > 0) {
         size_ = size;
         capacity_ = size;
         data_ = new float[size];
         std::fill(data_, data_ + size, 0);
     }
-    else if(size<0) {
-        throw::std::invalid_argument("Negative array size\n");
+    else if (size < 0) {
+        throw std::invalid_argument("Negative array size\n");
     }
     else if (size == 0) {
-        throw::std::invalid_argument("Array of zero size\n");
+        throw std::invalid_argument("Array of zero size\n");
     }
 }
 
-std::ptrdiff_t DynArr::size() const noexcept{
+std::ptrdiff_t DynArr::Size() const noexcept {
     return size_;
 }
 
@@ -54,12 +54,12 @@ DynArr& DynArr::DynArr::operator=(DynArr&& src) noexcept {
     return *this;
 }
 
-float& DynArr::operator[](const std::ptrdiff_t i){
+float& DynArr::operator[](const std::ptrdiff_t i) {
     if (i < 0) {
-        throw::std::invalid_argument("Negative index\n");
+        throw std::invalid_argument("Negative index\n");
     }
     else if (i >= size_) {
-        throw::std::out_of_range("Index out of range\n");
+        throw std::out_of_range("Index out of range\n");
     }
     else {
         return data_[i];
@@ -68,22 +68,22 @@ float& DynArr::operator[](const std::ptrdiff_t i){
 
 const float& DynArr::operator[](const std::ptrdiff_t i) const {
     if (i < 0) {
-        throw::std::invalid_argument("Negative index\n");
+        throw std::invalid_argument("Negative index\n");
     }
     else if (i >= size_) {
-        throw::std::out_of_range("Index out of range\n");
+        throw std::out_of_range("Index out of range\n");
     }
     else {
         return data_[i];
     }
 }
 
-void DynArr::resize(std::ptrdiff_t size) {
+void DynArr::Resize(std::ptrdiff_t size) {
     if (size > 0) {
         if (size > size_ && size <= capacity_) {
             std::fill(data_ + size_, data_ + size, 0);
         }
-        else if (size > capacity_){
+        else if (size > capacity_) {
             float* new_data = new float[size];
             std::copy(data_, data_ + size_, new_data);
             std::fill(new_data + size_, new_data + size, 0);
@@ -93,10 +93,10 @@ void DynArr::resize(std::ptrdiff_t size) {
         }
         size_ = size;
     }
-    else if(size<0) {
-        throw::std::invalid_argument("Negative array size\n");
+    else if (size < 0) {
+        throw std::invalid_argument("Negative array size\n");
     }
     else {
-        throw::std::invalid_argument("Array of zero size\n");
+        throw std::invalid_argument("Array of zero size\n");
     }
 }
