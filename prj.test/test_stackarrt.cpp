3bce91150f78f996e82bd2531ab6b4e235493349
basya <144015194+varyaqt@users.noreply.github.com>
2024-04-22 13:13:03 +0300

    Update test_stackarrt.cpp

--- prj.test/test_stackarrt.cpp
+++ prj.test/test_stackarrt.cpp
@@ -1,6 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include <stackarrt/stackarrt.hpp>
+#include "stackarrt/stackarrt.hpp"
 #include <cstdint>
 #include <string>
 #include <vector>
@@ -12,24 +12,23 @@ TEST_CASE("stackarrt ctor") {
     CHECK_EQ(1, stf.IsEmpty());
     CHECK_EQ(1, sti.IsEmpty());
     CHECK_EQ(1, sts.IsEmpty());
-	stf(4);
+	/*stf(4);
 	sti(4);
 	sts(4);
 	for (int i = 0; i < 4; i++) {
 		CHECK_EQ(T{}, stf[i]);
 		CHECK_EQ(T{}, sti[i]);
 		CHECK_EQ(T{}, sts[i]);
-	}
-	for (int i = 0, i < 100; i++) {
+	}*/
+	for (int i = 0; i < 100; i++) {
 		sti.Push(i);
 		CHECK_EQ(i, sti.Top());
 	}
-	for (int i = 0, i < 100; i++) {
+	for (int i = 0; i < 100; i++) {
 		stf.Push(i + 0.5);
 		CHECK_EQ(i + 0.5, sti.Top());
 	}
-	std::vector<std:string> tes;
-	tes = ["a", "s", "d", "f", "g", "h", "t", "a", "s", "d", "f", "g", "h", "t", "a", "s", "d", "f", "g", "h", "t"];
+	std::vector<std::string> tes = { "at", "st", "ds", "f", "fg", "h", "t", "a", "s", "fd", "f", "g", "h", "t", "sssssa", "s", "d", "f", "g", "h", "t" };
 	for (auto c : tes) {
 		sts.Push(c);
 		CHECK_EQ(c, sts.Top());
