38e0fb14b3d6febb887fd7706e7eb4d238a95150
basya <144015194+varyaqt@users.noreply.github.com>
2024-05-06 21:22:06 +0300

    Update stacklst.cpp

--- prj.lab/stacklst/stacklst.cpp
+++ prj.lab/stacklst/stacklst.cpp
@@ -1,103 +1,113 @@
-#include "queuelst/queuelst.hpp"
+#include "stacklst.hpp"
 
-#include <stdexcept>
-
-QueueLst::QueueLst(QueueLst&& obj) noexcept {
-    head_ = obj.head_;
-    tail_ = obj.tail_;
-    obj.head_ = nullptr;
-    obj.tail_ = nullptr;
+StackLst::StackLst(const StackLst& rhs) {
+    if (rhs.head_ != nullptr) {
+        Node* newNode = new Node(rhs.head_->v);
+        head_ = newNode;
+        Node* vs = rhs.head_->next;
+        Node* pr = newNode;
+        while (vs != nullptr) {
+            Node* newNode = new Node(vs->v);
+            pr->next = newNode;
+            vs = vs->next;
+            pr = newNode;
+        }
+    }
 }
 
-QueueLst& QueueLst::operator=(QueueLst&& rhs) noexcept {
+StackLst::StackLst(StackLst&& rhs) noexcept {
+    std::swap(head_, rhs.head_);
+}
+
+StackLst::~StackLst() {
+    while (head_ != nullptr) {
+        Node* delete_future = head_;
+        head_ = head_->next;
+        delete delete_future;
+    }
+}
+
+StackLst& StackLst::operator=(StackLst&& rhs) noexcept {
     if (this != &rhs) {
-        Clear();
-        head_ = rhs.head_;
-        tail_ = rhs.tail_;
-        rhs.head_ = nullptr;
-        rhs.tail_ = nullptr;
+        std::swap(head_, rhs.head_);
     }
     return *this;
 }
 
-bool QueueLst::IsEmpty() const noexcept {
-    return nullptr == head_;
-}
-
-void QueueLst::Pop() noexcept {
-    if (!IsEmpty()) {
-        Node* deleted = head_;
-        head_ = head_->next;
-        delete deleted;
-    }
-    if (IsEmpty()) {
-        tail_ = nullptr;
-    }
-}
-
-void QueueLst::Push(const Complex& val) {
-    if (IsEmpty()) {
-        tail_ = new Node{ val };
-        head_ = tail_;
-    }
-    else {
-        tail_->next = new Node{ val };
-        tail_ = tail_->next;
-    }
-}
-
-Complex& QueueLst::Top() {
-    if (IsEmpty()) {
-        throw std::logic_error("QueueLst - try get top from empty queue.");
-    }
-    return head_->v;
-}
-
-const Complex& QueueLst::Top() const {
-    if (IsEmpty()) {
-        throw std::logic_error("QueueLst - try get top form empty queue.");
-    }
-    return head_->v;
-}
-
-void QueueLst::Clear() noexcept {
-    while (!IsEmpty()) {
-        Pop();
-    }
-}
-
-QueueLst& QueueLst::operator=(const QueueLst& rhs) {
+StackLst& StackLst::operator=(const StackLst& rhs) noexcept {
     if (this != &rhs) {
         Node* vs = rhs.head_;
         Node* tvs = head_;
-        Node* pr = nullptr;
+        Node* pr;
         while (vs != nullptr) {
             if (tvs != nullptr) {
                 tvs->v = vs->v;
             }
             else {
                 Node* newNode = new Node(vs->v);
-                if (head_ == nullptr) {
-                    head_ = newNode;
-                    tail_ = newNode;
+                if (tvs != head_) {
+                    pr->next = newNode;
+                    tvs = newNode;
                 }
                 else {
-                    tail_->next = newNode;
-                    tail_ = tail_->next;
+                    head_ = newNode;
+                    tvs = newNode;
                 }
-                tvs = newNode;
             }
             pr = tvs;
-            tvs = tvs->next;
             vs = vs->next;
+            tvs = tvs->next;
         }
         while (tvs != nullptr) {
             Node* delete_future = tvs;
             tvs = tvs->next;
             delete delete_future;
         }
-        tail_ = pr;
-        if (tail_ != nullptr) tail_->next = nullptr;
+        pr->next = nullptr;
     }
     return *this;
+}
+
+void StackLst::Push(const Complex& c) {
+    Node* newNode = new Node(c);
+    newNode->next = head_;
+    head_ = newNode;
+}
+
+void StackLst::Pop() noexcept {
+    if (head_ != nullptr) {
+        Node* delete_future = head_;
+        head_ = head_->next;
+        delete delete_future;
+    }
+}
+
+bool StackLst::IsEmpty() const noexcept {
+    return (head_ == nullptr) ? 1 : 0;
+}
+
+Complex& StackLst::Top() {
+    if (head_ == nullptr) {
+        throw std::logic_error("Stack is empty!\n");
+    }
+    else {
+        return head_->v;
+    }
+}
+
+const Complex& StackLst::Top() const {
+    if (head_ == nullptr) {
+        throw std::logic_error("Stack is empty!\n");
+    }
+    else {
+        return head_->v;
+    }
+}
+
+void StackLst::Clear() noexcept {
+    while (head_ != nullptr) {
+        Node* delete_future = head_;
+        head_ = head_->next;
+        delete delete_future;
+    }
 }
