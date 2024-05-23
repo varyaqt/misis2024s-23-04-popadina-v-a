cb30e57454d8df5d8839f4dadfff057f807b164a
basya <144015194+varyaqt@users.noreply.github.com>
2024-05-23 19:08:55 +0300

    Update roborun.cpp

--- prj.app/roborun/roborun.cpp
+++ prj.app/roborun/roborun.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory>
+
 class Go {
 public:
-    std::vector<std::string> data;
-    std::string command;
     virtual void Go(double num) noexcept {
-        std::cin >> command;
-        std::cin >> num;
+        SetCmds.push_back(num);
     }
+    std::vector<double> SaveCmds;
     double GetNumber() { return num_; }
     double GetX() { return x_; }
     double GetY() { return y_; }
@@ -20,7 +20,7 @@ private:
     double num_ = 0;
     double x_ = 0;
     double y_ = 0;
-    
+    std::vector<std::unique_ptr <SaveCmds>> cmd;    
 };
 bool Go::IsEmpty() noexcept {
     if (data.size() == 0) return true;
@@ -42,32 +42,32 @@ void Go::REV(int num) {
  // y -
 class GoW : public Go {
 public:
-    void Go(double num) {
-        data.push_back("w" + static_cast<std::string>(num));
+    void Go(double num) override {
+        
     }
 };
 
 //x +
 class GoN : public Go {
 public:
-    void Go(double num) {
-        data.push_back("n" + static_cast<std::string>(num));
+    void Go(double num) override {
+        
     }
 };
 
 //y+
 class GoE : public Go {
 public:
-    void Go(double num) {
-        data.push_back("e" + static_cast<std::string>(num));
+    void Go(double num) override {
+        
     }
 };
 
 //x -
 class GoS : public Go {
 public:
-    void Go(double num) {
-        data.push_back("s" + static_cast<std::string>(num));
+    void Go(double num) override {
+        
     }
 };
 
