a7c7f10bba0dcfdfcd5e1b95f7c5abc8fc5bae6d
basya <144015194+varyaqt@users.noreply.github.com>
2024-06-09 16:30:43 +0300

    Update back.cpp

--- prj.cw/back.cpp
+++ prj.cw/back.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <vector>
+#include "colors.hpp"
 /*C:/Users/popad/Desktop/tests/darsie.jpg*/
 int main() {
     cv::Mat img;
@@ -37,17 +39,26 @@ int main() {
             rectangle(mosaic, cv::Rect(j / square_size * square_size, i / square_size * square_size, square.cols, square.rows), avg_color, -1);
         }
     }
+    std::vector<std::string> colors;
+    for (int i = 0; i < height; i += square_size) {
+        for (int j = 0; j < width; j += square_size) {
+            cv::Vec3b pixel_color = img.at<cv::Vec3b>(j, i);
+            cv::Vec3b rgb_color;
+            rgb_color[0] = pixel_color[2];
+            rgb_color[1] = pixel_color[1];
+            rgb_color[2] = pixel_color[0];
+
+            auto color_it = color_map.find(rgb_color);
+            if (color_it != color_map.end()) {
+                std::string thread_number = color_it->second;
+                colors.push_back(thread_number);
+            }
+        }
+    }
+    std::cout << "We recommend that you try using these thread colors:\n";
+    for (auto c : colors) std::cout << c << "\n";
     imshow("Мозаичное изображение", mosaic);
     imwrite("C:/Users/popad/Desktop/tests/result.jpg", mosaic);
     cv::waitKey(0);
     return 0;
-
-    /*std::cout << "What is the number of colors in the palette?\n";
-    int nColors = 0;
-    std::cin >> nColors;
-    while (nColors <= 0) {
-        std::cout << "The impossible number of colors. The number must be positive.\n";
-        std::cout << "What is the number of colors in the palette?\n";
-        std::cin >> nColors;
-    }*/
-}
+} 
